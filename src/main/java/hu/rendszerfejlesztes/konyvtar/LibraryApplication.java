77dd6feba4869044c143f9a1970bf7ae76819f40
Károly Pákozdi <karoly.pakozdi@frontendart.com>
2018-10-11 19:30:36 +0200

    Adding Procfile for Heroku, creating prod properties.

--- src/main/java/hu/rendszerfejlesztes/konyvtar/LibraryApplication.java
+++ src/main/java/hu/rendszerfejlesztes/konyvtar/LibraryApplication.java
@@ -2,37 +2,35 @@ package hu.rendszerfejlesztes.konyvtar;
 
 import hu.rendszerfejlesztes.konyvtar.model.Customer;
 import hu.rendszerfejlesztes.konyvtar.model.repositories.CustomerRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Arrays;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "hu.rendszerfejlesztes.konyvtar")
 @EnableJpaRepositories(basePackages = {"hu.rendszerfejlesztes.konyvtar.model.repositories"})
 @EnableConfigurationProperties
-public class Application {
+public class LibraryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(LibraryApplication.class, args);
     }
 
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository) {
-        return arg ->
-            customerRepository.saveAll(Arrays.asList(
-                    new Customer("Jack", "Bauer"),
-                    new Customer("Chloe", "O'Brian"),
-                    new Customer("Kim", "Bauer"),
-                    new Customer("David", "Palmer"),
-                    new Customer("Michelle", "Dessler")
-                    )
-            );
+        return arg -> customerRepository.saveAll(Arrays.asList(
+                new Customer("Jack", "Bauer"),
+                new Customer("Chloe", "O'Brian"),
+                new Customer("Kim", "Bauer"),
+                new Customer("David", "Palmer"),
+                new Customer("Michelle", "Dessler")
+                )
+        );
     }
 
 }
