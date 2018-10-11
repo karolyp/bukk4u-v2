82300fc228ef44e740a28e56add0626aa4501ca7
Pákozdi Károly <karoly.pakozdi@logmein.com>
2018-10-11 15:30:34 +0200

    Adding GenerationType to the Customer model.

--- src/main/java/hu/rendszerfejlesztes/konyvtar/model/Customer.java
+++ src/main/java/hu/rendszerfejlesztes/konyvtar/model/Customer.java
@@ -2,13 +2,14 @@ package hu.rendszerfejlesztes.konyvtar.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Customer {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
