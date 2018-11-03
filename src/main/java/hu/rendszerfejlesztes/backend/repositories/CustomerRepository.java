708abf1d975e6e916b3db849afb5f6cd51390fed
Károly Pákozdi <h668139@stud.u-szeged.hu>
2018-11-03 11:31:46 +0100

    Replacing base project with a simpler one.

--- src/main/java/hu/rendszerfejlesztes/backend/repositories/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.rendszerfejlesztes.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hu.rendszerfejlesztes.backend.data.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- src/main/java/hu/rendszerfejlesztes/backend/repositories/HistoryItemRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.rendszerfejlesztes.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hu.rendszerfejlesztes.backend.data.entity.HistoryItem;
-
-public interface HistoryItemRepository extends JpaRepository<HistoryItem, Long> {
-}
