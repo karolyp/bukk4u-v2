36444499d50a492c087943133efd9ccac68fa9e8
Vajda Alex <h670727@stud.u-szeged.hu>
2018-11-22 19:23:38 +0100

    Added home component

---
+++ app/konyvtar-ui/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home.component',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+
+export class HomeComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
